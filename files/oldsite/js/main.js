8e0783955ad8e4260f1330c71d0954dd22132010
ahmed samy alkhouly <ahmedthebest31@gmail.com>
2019-07-16 17:27:59 +0200

    new website "modern_portfolio" with sass
    new website "modern_portfolio" with sass

---
+++ files/oldsite/js/main.js
@@ -0,0 +1,20 @@
+//Option 2 - jQuery Smooth Scrolling
+$('.intro a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body')
+      .animate({
+        scrollTop: $(hash).offset().top
+      },800);
+  }
+});
+
+/*
+ Option 3 - Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
+ const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+	speed: 500
+});
+*/
--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
-//Option 2 - jQuery Smooth Scrolling
-$('.intro a').on('click', function (e) {
-  if (this.hash !== '') {
-    e.preventDefault();
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
-    const hash = this.hash;
+// Set Initial State Of Menu
+let showMenu = false;
 
-    $('html, body')
-      .animate({
-        scrollTop: $(hash).offset().top
-      },800);
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
   }
-});
-
-/*
- Option 3 - Smooth Scroll - https://github.com/cferdinandi/smooth-scroll
- const scroll = new SmoothScroll('.navbar a[href*="#"]', {
-	speed: 500
-});
-*/
+}
